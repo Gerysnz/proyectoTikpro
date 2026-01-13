a07d628c8c3de596983436f85bced8d158f066ae
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-13 22:19:59 +0100

    Update login.php

--- login.php
+++ login.php
@@ -3,9 +3,9 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $dsn = 'mysql:host=localhost;dbname=tikprop;charset=utf8';
-    $db_user = 'gery';
-    $db_pass = 'superlocal';
+    $dsn = 'mysql:host=localhost;dbname=project_platform;charset=utf8';
+    $db_user = 'adminsimbio';
+    $db_pass = 'AdminSimbi@26';
 
     try {
         $pdo = new PDO($dsn, $db_user, $db_pass);
