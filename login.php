d9a2b56c8ff76f04056e0da906ab3956364d1c8b
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-13 21:41:45 +0100

    prueba de ruta 2

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="/styles.css?=<?php echo time(); ?>">
+    <link rel="stylesheet" href="styles.css?=<?php echo time(); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Poppins:wght@500;700&display=swap" rel="stylesheet">
 
     <title>Document</title>
