82064b944dc0f097debcfa31aedc2fc9e351d110
xhector7 <loopsis753@gmail.com>
2026-01-13 19:16:45 +0100

    commit para merge

--- login.php
+++ login.php
@@ -3,26 +3,6 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $dsn = 'mysql:host=localhost;dbname=project_platform;charset=utf8';
-    $db_user = 'adminsimbio';
-    $db_pass = 'AdminSimbi@26';
-
-    try {
-        $pdo = new PDO($dsn, $db_user, $db_pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Error de conexión: " . $e->getMessage();
-        exit();
-    }
-
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
-    $password_hash = hash('sha256', $password);
-
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
-    $stmt->execute([$email]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
     // funcion LOGS
     function writeLog($mensaje) {
         $fechaArchivo = date("Y-m-d");
@@ -40,9 +20,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         file_put_contents($ruta, $linea, FILE_APPEND);
     }
 
-    if ($user && hash_equals($user['password'], $password_hash)) {
+    
+    $dsn = 'mysql:host=localhost;dbname=project_platform;charset=utf8';
+    $db_user = 'adminsimbio';
+    $db_pass = 'AdminSimbi@26';
 
-        // Guardamos datos en sesión
+    try {
+        $pdo = new PDO($dsn, $db_user, $db_pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        writeLog("Intento de login fallido a la base de datos");
+        echo "Error de conexión: " . $e->getMessage();
+        exit();
+    }
+
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $password_hash = hash('sha256', $password);
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    
+    if ($user && hash_equals($user['password'], $password_hash)) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_name'] = $user['name'];
 
@@ -57,7 +58,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Poppins:wght@500;700&display=swap" rel="stylesheet">
+
+    <title>Document</title>
+
+
+      
+</head>
+<body>
+     <header class="header">
+        Chamba
+    </header>
+    <main class="login-contenedor">
+        
+        <h2>Iniciar sesión</h2>
+        <form class="login-form" action="login.php" method="POST">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Contraseña</label>
+            <input type="password" id="password" name="password" required>
+            
+
+            <button type="submit">Entrar</button>
+        </form>
+    </main>
+
+
+    <script>
+        // Aquí puedes agregar cualquier script necesario para la página de login
+    </script>
+   
+    
+    
+</body>
+</html>
 
 
 
