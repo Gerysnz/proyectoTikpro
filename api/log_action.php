95e5f20f5fedce4d5b032dc696e8411c134c0020
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-14 17:52:05 +0100

    pruebas

--- api/log_action.php
+++ api/log_action.php
@@ -1,5 +1,5 @@
 <?php
-session_start("chamba");
+session_start();
 
 if (!isset($_SESSION['user_id'])) {
     http_response_code(401);
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-session_start("chamba");
+session_start();
 require_once __DIR__ . "/feedback.php";
 require_once __DIR__ . "/admin/logs.php";
 
