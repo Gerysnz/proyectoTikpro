c1ba50bc14b846820b8e1c34c30cef909a08a0e6
Gerysnz <gerardfuentesaf@gmail.com>
2026-01-20 19:58:57 +0100

    diseño base y estilos, falta q salga modal y llamada busqueda etc

--- profile.php
+++ profile.php
@@ -36,8 +36,19 @@ if (!isset($_SESSION['user_id'])) {
 <section class="tags" id="profile-tags">
     <h2>Etiquetes</h2>
     <!-- Etiquetas del usuario -->
-    <button disabled>+ Afegir</button>
+    <div id="user-tags-list"></div>
+    <button class="btn" id="btn-add-tag">+ Afegir</button>
 </section>
+
+<!-- Modal per afegir etiqueta -->
+<div id="modal-add-tag" class="modal-tag" style="display:none;">
+    <div class="modal-tag-content">
+        <span class="close-modal-tag">&times;</span>
+        <h3>Afegir etiqueta</h3>
+        <input type="text" id="input-search-tag" placeholder="Cerca família professional o cicle...">
+        <div id="search-tag-results"></div>
+    </div>
+</div>
 
 <section class="projects" id="profile-projects">
     <h2>Projectes propis</h2>
